6b9e441029806b904e44694d5219462c6c963dfe
seifmegahed <seifmegahed@me.com>
2024-09-10 01:58:23 +0300

    added csv and js support -- and added graph to readme

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # RSSI LUT
 
 A simple tool to generate a lookup map for RSSI values to percentage in ts.
-The script generates a file called `rssi-lut.ts` which can be used in a typescript project.
+The script generates a `rssi-lut.ts` and `rssi-lut.js` files which can be used in a js/ts project and an `rssi-lut.csv` file which can be used in a spreadsheet.
+
+You can adjust the `worst` and `best` values to change the range of the lookup map, and you can adjust the `delimiter` value to change the delimiter of the CSV file.
 
 Based on [linux's ipw2200](https://github.com/torvalds/linux/blob/9ff9b0d392ea08090cd1780fb196f36dbb586529/drivers/net/wireless/intel/ipw2x00/ipw2200.c#L4321) implementation.
+
+The graph below shows the dBm to RSSI plot for best and worst values of -30 and -85 respectively.
+
+![Graph Plot](images/graph.svg)
 
 ## Usage
 
--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const fs = require('fs');
+/**
+ * Generate a lookup map for RSSI values to percentage in ts.
+ * The script generates a `rssi-lut.ts` and `rssi-lut.js` files
+ * which can be used in a js/ts project
+ * and an `rssi-lut.csv` file which can be used in a spreadsheet.
+ *
+ * You can adjust the `worst` and `best` values to change the range of the lookup map,
+ * and you can adjust the `delimiter` value to change the delimiter of the CSV file.
+ */
+const best = -30; // The best RSSI value
+const worst = -85; // The worst RSSI value
+const delimiter = ","; // The delimiter of the CSV file
+
+const fs = require("fs");
 
 const signalQuality = (dbm, worst = -85, best = -30) =>
   Math.round(
@@ -7,15 +20,19 @@ const signalQuality = (dbm, worst = -85, best = -30) =>
       ((best - worst) * (best - worst))
   );
 
-let fileContent = "export const rssiLUT = new Map([\n";
+let tsFileContent = "export const rssiLUT = new Map([\n";
+let csvFileContent = `dbm${delimiter} rssi\n`;
 
 for (let i = -20; i >= -100; i--) {
-  fileContent += `  [${i},${Math.max(Math.min(signalQuality(i), 100), 0)}],\n`;
+  const rssi = Math.max(Math.min(signalQuality(i), 100), 0);
+  tsFileContent += `  [${i}, ${rssi}],\n`;
+  csvFileContent += `${i}${delimiter} ${rssi}\n`;
 }
 
-fileContent += "]);";
-fs.writeFileSync(
-  'rssi-lut.ts',
-  fileContent,
-);
+tsFileContent += "]);";
 
+fs.writeFileSync("./output/rssi-lut.csv", csvFileContent);
+
+fs.writeFileSync("./output/rssi-lut.ts", tsFileContent);
+
+fs.writeFileSync("./output/rssi-lut.js", tsFileContent);
